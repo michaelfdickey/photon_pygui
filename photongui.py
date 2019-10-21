2f2b29b28ff2f3060a37fc131b21bbcb438b59a1
matus1976 <matus1976@gmail.com>
2019-10-20 20:25:37 -0700

    starting adding button dictionary

--- photongui.py
+++ photongui.py
@@ -2,6 +2,8 @@
 #  	dev notes
 #	09-27-2019 - currently incorporating menu_physics_garage_005b.py - DONE
 #	10-19-2019 - currently integrating menu_physics_garage_006.py
+#		- working on getting buttons to change color when clicked, and change back when clicked again
+#		- each button needs a dictionary
 
 
 # ************************************************************************************
@@ -94,8 +96,9 @@ def findButton(buttons, x, y):
 # ************************************************************************************
 #	classes		#
 
+## takes button info and prepares it for displaying
 class Button:
-	def __init__ (self, (x,y), x_width, y_height, label_txt, buttonType):
+	def __init__ (self, (x,y), x_width, y_height, label_txt, buttonType, buttonEnabled):
 		self.x = x
 		self.x_width = x_width
 		self.y = y
@@ -105,6 +108,21 @@ class Button:
 		self.label_txt = label_txt
 		self.colorBorder = UI_button_border_color
 		self.buttonType = buttonType  #pushy, sticky, checky, label (label is just text, not a button)
+		self.buttonEnabled = buttonEnabled
+
+	## displays buttons
+	def display(self):
+		#pygame.draw.circle(screen, self.color, (int(self.x),int(self.y)), self.size, self.thickness)
+		pygame.draw.rect(screen, self.color, (self.x, self.y, self.x_width, self.y_height))               		#button
+		pygame.draw.rect(screen, self.colorBorder, (self.x, self.y, self.x_width, self.y_height), 3)  	#border
+
+		label = myfont.render(str(self.label_txt), 0, UI_button_txt_color)
+		screen.blit(label, (self.x + 5, self.y))
+
+## updates button display status
+class ButtonUpdate:
+	def __init__ (self, buttonEnabled):
+		self.buttonEnabled = buttonEnabled
 
 	def display(self):
 		#pygame.draw.circle(screen, self.color, (int(self.x),int(self.y)), self.size, self.thickness)
@@ -113,7 +131,7 @@ class Button:
 
 		label = myfont.render(str(self.label_txt), 0, UI_button_txt_color)
 		screen.blit(label, (self.x + 5, self.y))
-		#screen.blit(label, (10, (pygame_window_height-20)))
+
 
 # ************************************************************************************
 
@@ -126,15 +144,46 @@ my_buttons = []
 
 for n in range(1):
 
+	# create sticky01 button
+	button_name =  "Sticky01"
+	button_origin_x = 0								#x0, y0 is upper left corner
+	button_origin_y = pygame_window_height - 100
+	button_width = UI_sideBar_width
+	button_height = 20
+	button_label_txt = "Sticky Button 01"
+	buttonType = "sticky"
+	buttonEnabled = False 
+	
+	# define button
+	created_button = Button((button_origin_x,button_origin_y), button_width, button_height, button_label_txt, buttonType, buttonEnabled)
+	# add button to display list
+	my_buttons.append(created_button)
+	print "button origin x", button_origin_x, "button width pos", button_origin_x + button_width
+
+	# trying to create button dictionary
+	sticky01 = {}
+	sticky01[0] = button_name
+	sticky01[1] = button_origin_x
+	sticky01[2] = button_origin_y
+	sticky01[3] = button_width
+	sticky01[4] = button_height
+	sticky01[5] = button_label_txt
+	sticky01[6] = buttonType
+	sticky01[7] = buttonEnabled
+
+	print "sticky01 button dictionary"
+	print sticky01
+
 	# create command01 button
 	button_cmd01_origin_x = 0								#x0, y0 is upper left corner
 	button_cmd01_origin_y = pygame_window_height - 60
 	button_cmd01_width = UI_sideBar_width
 	button_cmd01_height = 20
 	label_txt = "Command 01"
-	buttonType = "sticky"
+	buttonType = "pushy"
+	button_cmd01_Enabled = False 
 	# define button
-	created_button = Button((button_cmd01_origin_x,button_cmd01_origin_y), button_cmd01_width, button_cmd01_height, label_txt, buttonType)
+	created_button = Button((button_cmd01_origin_x,button_cmd01_origin_y), button_cmd01_width, button_cmd01_height, label_txt, buttonType, button_cmd01_Enabled)
 	# add button to dictionary
 	my_buttons.append(created_button)
 	print "button origin x", button_cmd01_origin_x, "button width pos", button_cmd01_origin_x + button_cmd01_width
@@ -146,9 +195,10 @@ for n in range(1):
 	button_exit_width = UI_sideBar_width
 	button_exit_height = 20
 	label_txt = "EXIT"
-	buttonType = "sticky"
+	buttonType = "pushy"
+	button_exit_Enabled = False
 	# define button
-	created_button = Button((button_exit_origin_x,button_exit_origin_y), button_exit_width, button_exit_height, label_txt, buttonType)
+	created_button = Button((button_exit_origin_x,button_exit_origin_y), button_exit_width, button_exit_height, label_txt, buttonType, button_exit_Enabled)
 	# add button to dictionary
 	my_buttons.append(created_button)
 	print "button origin x", button_exit_origin_x, "button width pos", button_exit_origin_x + button_exit_width
@@ -185,12 +235,6 @@ while running:
 	pygame.draw.rect(screen, UI_background_color, (0, 0, pygame_window_width, UI_topBar_height))
 	pygame.draw.rect(screen, UI_background_color, (0,0, UI_sideBar_width, pygame_window_height))
 
-
-	# # draw buttons!
-	for i, button in enumerate(my_buttons):
-		button.display()
-
-
 	# pygame event monitoring
 	# pygame scans for mouse and keyboard events and takes actions accordingly
 
@@ -211,13 +255,31 @@ while running:
 
 	if selected_button != None:
 		(mouseX, mouseY) = pygame.mouse.get_pos()
-		selected_button.color = (255,128,255)
+		selected_button.color = UI_button_click_color
 		print selected_button.color
 		pygame.display.flip()
 
 		if selected_button.label_txt == "EXIT":
 			print "you pressed exit"
 			running = False
+
+		if selected_button.buttonType == "pushy":
+			selected_button.color = UI_button_click_color
+
+		if selected_button.buttonType == "sticky":
+			if selected_button.buttonEnabled == False:
+				selected_button.color == UI_button_selected_color
+				# update the button status
+				buttonEnabled = True 
+				updated_button = Button(buttonEnabled)
+				my_buttons.append(updated_button)				
+
+
+				#selected_button.buttonEnabled = True
+
+	# # draw buttons!
+	for i, button in enumerate(my_buttons):
+		button.display()
 
 
 	pygame.display.flip()		 # updates and draws the screen & launch pygame window
