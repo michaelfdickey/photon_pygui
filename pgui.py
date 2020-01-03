15d20e37fe94381114ceecfda00f78eea9145ce4
michaelfdickey <michaelfdickey@github.com>
2020-01-02 22:43:49 -0800

    add first menu option 'monday'

--- pgui.py
+++ pgui.py
@@ -474,24 +474,35 @@ textField01["height"] = 20									# button_height
 textField01["label_txt"] = "abc123"							# button_label_txt
 textField01["type"] = "textEntry"								# buttonType
 textField01["enabled"] = False								# buttonEnabled
-textField01["color"] = pgvar.UI_text_entry_box_color				# buttonColor
+textField01["color"] = pgvar.UI_text_entry_box_color			# buttonColor
 textField01["group"] = "text01"								# buttonGroup
 textField01["visible"] = True									# buttonVisible
 
 bMenu01 = {}
-bMenu01["name"] = "menu01"							# button_name
-bMenu01["origin_x"] = pgvar.UI_sideBar_width				# button_origin_x
-bMenu01["origin_y"] = 0								# button_origin_y
-bMenu01["width"] = 125								# button_width
-bMenu01["height"] = 20								# button_height
-bMenu01["label_txt"] = " Menu 01 Sticky"				# button_label_txt
-bMenu01["type"] = "menu"								# buttonType
-bMenu01["enabled"] = False							# buttonEnabled
-bMenu01["color"] = pgvar.UI_button_color					# buttonColor
-bMenu01["group"] = "menu01"							# buttonGroup
-bMenu01["visible"] = True							# buttonVisible
+bMenu01["name"] = "menu01"									# button_name
+bMenu01["origin_x"] = pgvar.UI_sideBar_width					# button_origin_x
+bMenu01["origin_y"] = 0										# button_origin_y
+bMenu01["width"] = 150										# button_width
+bMenu01["height"] = 20										# button_height
+bMenu01["label_txt"] = " Menu 01 Sticky"						# button_label_txt
+bMenu01["type"] = "menu"										# buttonType
+bMenu01["enabled"] = False									# buttonEnabled
+bMenu01["color"] = pgvar.UI_button_color						# buttonColor
+bMenu01["group"] = "menu01"									# buttonGroup
+bMenu01["visible"] = True										# buttonVisible
 
-
+bMenu01option01 = {}
+bMenu01option01["name"] = "menu01option01"					# button_name
+bMenu01option01["origin_x"] = pgvar.UI_sideBar_width			# button_origin_x
+bMenu01option01["origin_y"] = 20								# button_origin_y
+bMenu01option01["width"] = 150								# button_width
+bMenu01option01["height"] = 20								# button_height
+bMenu01option01["label_txt"] = " Monday "						# button_label_txt
+bMenu01option01["type"] = "menu"								# buttonType
+bMenu01option01["enabled"] = False							# buttonEnabled
+bMenu01option01["color"] = pgvar.UI_button_color				# buttonColor
+bMenu01option01["group"] = "menu01"							# buttonGroup
+bMenu01option01["visible"] = True								# buttonVisible
 
 
 allButtons = {}
@@ -526,6 +537,7 @@ allButtons[27] = bDropdown01option01 	# Dropdown01 option 01
 allButtons[28] = bDropdown01option02 	# Dropdown01 option 02
 allButtons[29] = bDropdown01option03 	# Dropdown01 option 03
 allButtons[30] = bMenu01				# Menu 01
+allButtons[31] = bMenu01option01		# Menu 01 - option 01 - Monday
 
 allButtons[38] = lTextField01			# Text Field - label
 allButtons[39] = textField01			# Text Field - text entry box
